484b0eb4e55030b9ed17cc42ce08239024a9b689
Hassan Ali <helfi92@gmail.com>
2019-12-18 09:29:39 -0500

    Generate schema json

--- infrastructure/tooling/src/generate/generators/schema-json.js
+++ infrastructure/tooling/src/generate/generators/schema-json.js
@@ -1,34 +1,17 @@
 const { Schema } = require('taskcluster-lib-postgres');
-
+const sortObject = require('deep-sort-object');
+const { writeRepoJSON } = require('../../utils');
 // Generate a readable JSON version of the schema.
 exports.tasks = [{
   title: 'Schema JSON',
   requires: [],
   provides: ['schema-json'],
   run: async (requirements, utils) => {
-    // 
-    const refs = References.fromSerializable({serializable: requirements['references-json']});
+    const schema = Schema.fromDbDirectory();
 
-    const apis = {};
-    refs.references.forEach(({filename, content}) => {
-      const refSchema = refs.getSchema(content.$schema);
-
-      if (refSchema.metadata.name !== 'api' && refSchema.metadata.name !== 'exchanges') {
-        return; // ignore this reference
-      }
-      if (refSchema.metadata.version !== 0) {
-        throw new Error(`Unknown reference version in ${filename}`);
-      }
-
-      // invent a CamelCase name
-      const camelCaseName = content.serviceName
-        .split('-')
-        .concat(refSchema.metadata.name === 'exchanges' ? ['events'] : [])
-        .map(w => `${w[0].toUpperCase()}${w.slice(1)}`)
-        .join('');
-      apis[camelCaseName] = {reference: content};
-    });
-
-    return {apis};
+    writeRepoJSON('generated/schema.json', sortObject({
+      versions: schema.versions,
+      access: schema.access,
+    }, null, 2));
   },
 }];
