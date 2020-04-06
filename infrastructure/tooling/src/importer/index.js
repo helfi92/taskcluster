8ccd8262222b3dcbdaf8ad937fbca39a2bd1a569
Hassan Ali <helfi92@gmail.com>
2020-04-06 19:21:20 -0400

    Add heapdump feature
    kill -SIGUSR2 <pid> to create a heapdump file in the working directory.

--- infrastructure/tooling/src/importer/index.js
+++ infrastructure/tooling/src/importer/index.js
@@ -1,5 +1,6 @@
 const {TaskGraph, Lock} = require('console-taskgraph');
 const { Database } = require('taskcluster-lib-postgres');
+const heapdump = require('heapdump');
 const importer = require('./importer');
 const verifier = require('./verifier');
 const { requireEnv } = require('./util');
@@ -15,6 +16,10 @@ const main = async ({ operation }) => {
     },
   };
   const db = new Database({ urlsByMode: {admin: credentials.postgres.adminDbUrl}, statementTimeout: false });
+
+  process.once('SIGUSR2', function () {
+    heapdump.writeSnapshot(new Date().toJSON() + '.heapsnapshot');
+  });
 
   let tasks;
   if (operation === 'importer') {
