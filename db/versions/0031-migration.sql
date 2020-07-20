d6552a9400c1d8dddad9c187d5882d36a9690595
Hassan Ali <helfi92@gmail.com>
2020-07-20 10:54:08 -0400

    Add hashed_session_id to db (primary key)

--- db/versions/0031-migration.sql
+++ db/versions/0031-migration.sql
@@ -1,11 +1,12 @@
 begin
 
   create table sessions(
+    hashed_session_id text not null,
     encrypted_session_id jsonb not null,
     data jsonb not null,
     expires timestamptz not null
   );
-  alter table sessions add primary key (encrypted_session_id);
+  alter table sessions add primary key (hashed_session_id);
   grant select, insert, update, delete on sessions to $db_user_prefix$_web_server;
 
   revoke select, insert, update, delete on session_storage_table_entities from $db_user_prefix$_web_server;
